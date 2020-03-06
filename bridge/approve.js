4fdeb361e2ac44215f52b2c491002e2c85ef0e0c
Michael Feldstein <msfeldstein@gmail.com>
2020-03-06 11:40:02 -0800

    Add ability to revoke and reenable access

--- bridge/approve.js
+++ bridge/approve.js
@@ -5,81 +5,88 @@ const server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
 const issuer = StellarSdk.Keypair.fromSecret(process.env.ISSUER_SECRET);
 
 module.exports = async function(req, res, next) {
-	console.log(req.query.tx);
-	const envelope = StellarSdk.xdr.TransactionEnvelope.fromXDR(
-		req.query.tx,
-		"base64"
-	);
-	const tx = new StellarSdk.Transaction(envelope, StellarSdk.Networks.TESTNET);
-	let totalAmount = 0;
-	let assetsToParticipantMap = {};
+  console.log(req.query.tx);
+  const envelope = StellarSdk.xdr.TransactionEnvelope.fromXDR(
+    req.query.tx,
+    "base64"
+  );
+  const tx = new StellarSdk.Transaction(envelope, StellarSdk.Networks.TESTNET);
+  let totalAmount = 0;
+  let assetsToParticipantMap = {};
+  console.log("Checking operations in proposed transaction");
+  tx.operations.forEach(operation => {
+    console.log("  Type: " + operation.type);
+    console.log("  Source: " + (operation.source || tx.source));
+    console.log("  Destination: " + operation.destination);
+    console.log("  Asset: " + operation.asset.getCode());
+    console.log("  Amount: " + operation.amount);
+    if (operation.type === "payment") {
+      const code = operation.asset.getCode();
+      assetsToParticipantMap[code] = assetsToParticipantMap[code] || new Set();
+      totalAmount += parseFloat(operation.amount);
+      assetsToParticipantMap[code].add(operation.source || tx.source);
+      assetsToParticipantMap[code].add(operation.destination);
+    }
+  });
+  console.log("Total Amount: " + totalAmount);
+  console.log("<<< Consulting Rules Engine API >>>");
+  await new Promise(res => setTimeout(res, 1300));
+  if (totalAmount > 50) {
+    console.log("Rejecting, amount over 50 REG limit");
+    res.send({
+      status: "rejected",
+      error: "Amount over 50 REG limit"
+    });
+    return;
+  } else {
+    const [sourceAccount, feeStats] = await Promise.all([
+      server.loadAccount(tx.source),
+      server.feeStats()
+    ]);
+    console.log("Building revised sandwiched transaction");
+    const sandwichTxBuilder = new StellarSdk.TransactionBuilder(sourceAccount, {
+      fee: feeStats.fee_charged.p90,
+      networkPassphrase: StellarSdk.Networks.TESTNET
+    });
 
-	tx.operations.forEach(operation => {
-		console.log("Type: " + operation.type);
-		console.log("Source: " + operation.source);
-		console.log("Destination: " + operation.destination);
-		console.log("Asset: " + operation.asset.getCode());
-		console.log("Amount: " + operation.amount);
-		if (operation.type === "payment") {
-			const code = operation.asset.getCode();
-			assetsToParticipantMap[code] = assetsToParticipantMap[code] || new Set();
-			totalAmount += parseFloat(operation.amount);
-			assetsToParticipantMap[code].add(operation.source || tx.source);
-			assetsToParticipantMap[code].add(operation.destination);
-		}
-	});
-	console.log("Total Amount: " + totalAmount);
-	if (totalAmount > 50) {
-		res.send({
-			status: "rejected",
-			error: "Amount over 50 REG limit"
-		});
-	} else {
-		const [sourceAccount, feeStats] = await Promise.all([
-			server.loadAccount(tx.source),
-			server.feeStats()
-		]);
-		const sandwichTxBuilder = new StellarSdk.TransactionBuilder(sourceAccount, {
-			fee: feeStats.fee_charged.p90,
-			networkPassphrase: StellarSdk.Networks.TESTNET
-		});
+    const setParticipantAuthorizations = allow => {
+      Object.keys(assetsToParticipantMap).forEach(asset => {
+        const participants = assetsToParticipantMap[asset];
+        participants.forEach(participantAddress => {
+          console.log(
+            `  ${
+              allow ? "Allowing" : "Revoking"
+            } asset ${asset} for participant ${participantAddress}`
+          );
+          sandwichTxBuilder.addOperation(
+            StellarSdk.Operation.allowTrust({
+              trustor: participantAddress,
+              assetCode: asset,
+              authorize: allow,
+              source: issuer.publicKey()
+            })
+          );
+        });
+      });
+    };
 
-		const setParticipantAuthorizations = allow => {
-			Object.keys(assetsToParticipantMap).forEach(asset => {
-				const participants = assetsToParticipantMap[asset];
-				participants.forEach(participantAddress => {
-					console.log(
-						`${
-							allow ? "Allowing" : "Revoking"
-						} asset ${asset} for participant ${participantAddress}`
-					);
-					sandwichTxBuilder.addOperation(
-						StellarSdk.Operation.allowTrust({
-							trustor: participantAddress,
-							assetCode: asset,
-							authorize: allow,
-							source: issuer.publicKey()
-						})
-					);
-				});
-			});
-		};
-
-		setParticipantAuthorizations(true);
-		envelope
-			.tx()
-			.operations()
-			.forEach(op => sandwichTxBuilder.addOperation(op));
-		setParticipantAuthorizations(false);
-		sandwichTxBuilder.setTimeout(30);
-		const revisedTx = sandwichTxBuilder.build();
-		revisedTx.sign(issuer);
-		res.send({
-			status: "revised",
-			tx: revisedTx
-				.toEnvelope()
-				.toXDR()
-				.toString("base64")
-		});
-	}
+    setParticipantAuthorizations(true);
+    console.log("  Adding operations from original transaction ");
+    envelope
+      .tx()
+      .operations()
+      .forEach(op => sandwichTxBuilder.addOperation(op));
+    setParticipantAuthorizations(false);
+    sandwichTxBuilder.setTimeout(30);
+    const revisedTx = sandwichTxBuilder.build();
+    revisedTx.sign(issuer);
+    console.log("Approved, sending revised transaction back to wallet");
+    res.send({
+      status: "revised",
+      tx: revisedTx
+        .toEnvelope()
+        .toXDR()
+        .toString("base64")
+    });
+  }
 };
--- bridge/index.js
+++ bridge/index.js
@@ -1,20 +1,24 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const PORT = process.env.BRIDGE_PORT || 3000;
+const bodyParser = require("body-parser");
+const PORT = process.env.BRIDGE_PORT || 3001;
 
 const app = express();
 app.use(cors());
+app.use(bodyParser());
 
 app.get("/.well-known/stellar.toml", require("./toml"));
 app.get("/approve", require("./approve"));
 app.get("/deposit", require("./deposit"));
+app.get("/list", require("./list"));
+app.post("/revoke", require("./revoke"));
 
 app.use(function(err, req, res, next) {
-	console.error(err.stack);
-	res.status(500).send({ error: err });
+  console.error(err.stack);
+  res.status(500).send({ error: err });
 });
 
 app.listen(PORT, () => {
-	console.log(`Bridge listening on port ${PORT}`);
+  console.log(`Bridge listening on port ${PORT}`);
 });
